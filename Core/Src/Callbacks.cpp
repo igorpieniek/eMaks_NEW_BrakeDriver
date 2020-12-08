c7593fad976e202fc0a096568c9633020cc5d45d
igorpieniek <pieniek97@gmail.com>
2020-12-08 14:05:00 +0100

    Add callback function

--- Core/Src/Callbacks.cpp
+++ Core/Src/Callbacks.cpp
@@ -2,6 +2,7 @@
 #include "main.h"
 #include "AllTasks.h"
 #include "Futaba.h"
+#include "gpio.h"
 
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
   if (htim->Instance == TIM3) {
@@ -12,5 +13,17 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
 void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 {
 	if (huart->Instance == USART2) futaba.RxCallback();
+
+}
+
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin){
+	if(GPIO_Pin == LIMIT_SWITCH_LOW_Pin){
+		//TODO
+	}
+	if(GPIO_Pin == LIMIT_SWITCH_HIGH_Pin){
+		//TODO
+	}
+
 
 }

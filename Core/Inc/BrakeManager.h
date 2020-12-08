4d649bf07ac47d612feed148e782c62ee672e2a8
igorpieniek <pieniek97@gmail.com>
2020-12-08 14:05:55 +0100

    Add brake manager class files

---
+++ Core/Inc/BrakeManager.h
@@ -0,0 +1,17 @@
+/*
+ * BrakeManager.h
+ *
+ *  Created on: 08.12.2020
+ *      Author: Igor
+ */
+
+#ifndef INC_BRAKEMANAGER_H_
+#define INC_BRAKEMANAGER_H_
+
+class BrakeManager {
+public:
+	BrakeManager();
+	virtual ~BrakeManager();
+};
+
+#endif /* INC_BRAKEMANAGER_H_ */
---
+++ Core/Src/BrakeManager.cpp
@@ -0,0 +1,18 @@
+/*
+ * BrakeManager.cpp
+ *
+ *  Created on: 08.12.2020
+ *      Author: Igor
+ */
+
+#include <BrakeManager.h>
+
+BrakeManager::BrakeManager() {
+	// TODO Auto-generated constructor stub
+
+}
+
+BrakeManager::~BrakeManager() {
+	// TODO Auto-generated destructor stub
+}
+

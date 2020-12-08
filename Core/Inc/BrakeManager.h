83552dc59c9fb4c3d7f20df062cacfe111cba9d9
igorpieniek <pieniek97@gmail.com>
2020-12-08 15:14:56 +0100

    Add definition of some fuctions in brake manager
    -getState - based of current pin states
    -update piston state- to read and update initial state of piston

--- Core/Inc/BrakeManager.h
+++ Core/Inc/BrakeManager.h
@@ -14,13 +14,13 @@
 class BrakeManager {
 public:
 
-	enum class Limit_switch_flag{
-		RAISED, /**< flag raised - piston reached switch*/
-		DOWN    /**< flag down - normal state*/
+	enum  Limit_switch_flag{
+		RESET ,   /**< flag down - normal state*/
+		SET /**< flag raised - piston reached switch*/
 	};
-	enum Limit_switch{LOW,HIGH};
+	enum  Limit_switch{LOW,HIGH};
 
-	enum class Brake_state{
+	enum  Brake_state{
 		ZERO, 		/**< piston in minimum position*/
 		RELEASING, 	/**< piston is returning - releasing brake, temporary state*/
 		BRAKING,	/**< piston is moving - braking process, temporary state */
@@ -29,17 +29,23 @@ public:
 
 	void on();
 	void off();
+	void interrupt_update(Limit_switch sw); /**< update function to be called by EXTI interrupt*/
 
 
 	BrakeManager();
 	virtual ~BrakeManager();
 
 private:
+	enum Direction{ UP, DOWN};
+	enum Enable{OFF, ON};
+
 	Limit_switch_flag min_flag;
 	Limit_switch_flag max_flag;
 	Brake_state brake_state;
 
 	void stop();
+	Limit_switch_flag getState(Limit_switch);
+	void update_piston_state();
 
 };
 
--- Core/Src/BrakeManager.cpp
+++ Core/Src/BrakeManager.cpp
@@ -8,7 +8,9 @@
 #include <BrakeManager.h>
 
 BrakeManager::BrakeManager() {
-	// TODO Auto-generated constructor stub
+	//update current brake state - initial position of piston
+	update_piston_state();
+	//zeroing pins
 
 }
 
@@ -16,3 +18,38 @@ BrakeManager::~BrakeManager() {
 	// TODO Auto-generated destructor stub
 }
 
+void BrakeManager::update_piston_state(){
+	min_flag = getState(LOW);
+	max_flag = getState(HIGH);
+
+	if 		(min_flag == SET && max_flag == RESET) brake_state = ZERO;
+	else if	(min_flag == RESET && max_flag == SET) brake_state = MAX;
+	else if 	(min_flag == RESET && max_flag == RESET){
+		off(); // back to ZERO state
+	}
+	else stop(); //something wrong
+
+
+}
+
+void BrakeManager::stop(){
+
+
+}
+
+
+BrakeManager::Limit_switch_flag BrakeManager::getState(Limit_switch sw){
+	GPIO_PinState state = GPIO_PIN_RESET;
+	if (sw == LOW){
+		state = HAL_GPIO_ReadPin(LIMIT_SWITCH_LOW_GPIO_Port, LIMIT_SWITCH_LOW_Pin);
+	}
+	else if(sw == HIGH){
+		state = HAL_GPIO_ReadPin(LIMIT_SWITCH_HIGH_GPIO_Port, LIMIT_SWITCH_HIGH_Pin);
+	}
+
+	if(state == GPIO_PIN_RESET) return RESET;
+	else						return SET;
+
+
+
+}
